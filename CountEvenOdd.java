ab2cd2f0ea4a5fd6f5939d46fc81a2f94ed36aad
Itishri Sahoo <itishree1803@gmail.com>
2025-03-04 23:45:12 +0530

    Add CountEvenOdd file

--- CountEvenOdd.java
+++ CountEvenOdd.java
@@ -1,1 +1,34 @@
+import java.util.Scanner;
 
+public class CountEvenOddUserInput 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+      
+        System.out.print("Enter the number of elements: ");
+        int size = scanner.nextInt();
+
+        int[] numbers = new int[size];
+      
+        System.out.println("Enter " + size + " numbers:");
+        for (int i = 0; i < size; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        int evenCount = 0, oddCount = 0;
+      
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+      
+        System.out.println("Even numbers: " + evenCount);
+        System.out.println("Odd numbers: " + oddCount);
+
+        scanner.close();
+    }
+}
