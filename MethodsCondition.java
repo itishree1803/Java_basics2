2d64bb194ce41efe7fd774f8f8ff618436dac063
Itishri Sahoo <itishree1803@gmail.com>
2025-03-14 22:45:02 +0530

    Add MethodsCondition file

--- MethodsCondition.java
+++ MethodsCondition.java
@@ -1,1 +1,33 @@
 
+public class methodsCondition {
+    static int logic(int x, int y) {
+        int z;
+        if (x>y){
+            z = x + y;
+        }
+        else {
+            z = (x + y) * 5;
+        }
+        return z;
+    }
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 15;
+        int c;
+        c = logic(a , b);
+        System.err.println(c);
+
+        int a1 = 30;
+        int b1 = 25;
+        int c1;
+        c1 = logic(a1 , b1);
+        System.err.println(c1);
+
+        int a2 = 40;
+        int b2 = 15;
+        int c2;
+        c2 = logic(a2 , b2);
+        System.err.println(c2);
+    }
+}
